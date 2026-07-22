332fbd91896f8d0973471d170a672c42074b5f99
Chris <110760360+ckt00@users.noreply.github.com>
2026-07-21 20:50:23 -0400

    Add: python-dict-looping - submission-0

---
+++ Python For Beginners/python-dict-looping/submission-0.py
@@ -0,0 +1,23 @@
+from typing import Dict, List # this adds type hints for List and Dict
+
+def get_dict_keys(age_dict: Dict[str, int]) -> List[str]:
+    x = []
+    for k in age_dict:
+        x.append(k)
+    return x
+
+def get_dict_values(age_dict: Dict[str, int]) -> List[int]:
+    x = []
+    for k, v in  age_dict.items():
+        x.append(v)
+    return x
+
+# do not modify below this line
+dict_1 = {"John": 25, "Doe": 30, "Jane": 22}
+dict_2 = {"NeetCode": 24, "NeetCode2": 25, "NeetCode3": 26}
+
+print(get_dict_keys(dict_1))
+print(get_dict_keys(dict_2))
+
+print(get_dict_values(dict_1))
+print(get_dict_values(dict_2))
